eda2d52b3f0f8c6f19c4aa37c560940c7eece29f
3dfernando <fzigunov@gmail.com>
2016-12-01 11:22:14 -0200

    Began something

---
+++ Benchmarking.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBenchmark
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdRun = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmdRun
+        '
+        Me.cmdRun.Location = New System.Drawing.Point(12, 9)
+        Me.cmdRun.Name = "cmdRun"
+        Me.cmdRun.Size = New System.Drawing.Size(84, 34)
+        Me.cmdRun.TabIndex = 0
+        Me.cmdRun.Text = "Run Test"
+        Me.cmdRun.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(114, 25)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(275, 312)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(111, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Benchmarking Test Results:"
+        '
+        'frmBenchmark
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(406, 349)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.cmdRun)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmBenchmark"
+        Me.Text = "Benchmarking Testing"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmdRun As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Benchmarking.vb
@@ -0,0 +1,44 @@
+﻿Public Class frmBenchmark
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmdRun.Click
+        'Dim n As Integer = 10000000
+        'Dim i As Integer
+        'Dim a(), b(), c(), d() As Double
+        'ReDim a(n - 1)
+        'ReDim b(n - 1)
+        'ReDim c(n - 1)
+        'ReDim d(n - 1)
+
+        'For i = 0 To n - 1
+        '    a(i) = Rnd()
+        '    b(i) = Rnd()
+        '    c(i) = Rnd()
+        '    d(i) = Rnd()
+        'Next
+
+        'Solve_Tridiag(a, b, c, d)
+
+        Dim k1 As Double = 0.4
+        Dim k2 As Double = Rnd()
+        Dim r As Double
+        Dim i As Long
+        Dim N As Long = 100000000
+        Dim j() As Double
+        Dim b As Boolean = False
+
+        Dim t1 As DateTime = DateTime.Now() '<<Used to measure time performance>>
+        ReDim j(N)
+
+        For i = 1 To N
+            If k1 > 0.5 Then
+                r = k1 * k1
+            Else
+                r = k1
+            End If
+        Next
+
+        Dim t2 As DateTime = DateTime.Now() '<<Used to measure time performance>>
+        Dim dt As TimeSpan = t2 - t1 '<<Used to measure time performance>>
+
+        MsgBox(Trim(Str((dt.TotalMilliseconds / (N * 1000)) * 1000000000)) & " ns")
+    End Sub
+End Class
